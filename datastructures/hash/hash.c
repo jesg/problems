b33ae1c001529ff9591b0f1696d6d1049b17a92f
jesg <gowanjason@gmail.com>
2015-08-29 06:14:12 -0500

    create two lists
    each list has its own head

--- datastructures/hash/hash.c
+++ datastructures/hash/hash.c
@@ -4,25 +4,38 @@
 #include <stdlib.h>
 #include <sys/queue.h>
 
-SLIST_HEAD(slisthead, entry) head = SLIST_HEAD_INITIALIZER(head);
-
 typedef struct entry {
 	int value;
-	SLIST_ENTRY(entry) entries;     /* Singly-linked List. */
+	SLIST_ENTRY(entry) entries;
 } entry;
 
 int main(void){
 
-	SLIST_INIT(&head);                      /* Initialize the list. */
+	SLIST_HEAD(slisthead, entry) head = SLIST_HEAD_INITIALIZER(head);
+	SLIST_HEAD(slisthead2, entry) head2 = SLIST_HEAD_INITIALIZER(head2);
+	SLIST_INIT(&head);
+	SLIST_INIT(&head2);
 
 	for(int i=0; i<10; i++) {
-		entry *n1 = malloc(sizeof(struct entry));      /* Insert at the head. */
+		entry *n1 = malloc(sizeof(struct entry));
 		n1->value = i;
 		SLIST_INSERT_HEAD(&head, n1, entries);
 	}
 
+	for(int i=10; i>0; i--) {
+		entry *n1 = malloc(sizeof(struct entry));
+		n1->value = i;
+		SLIST_INSERT_HEAD(&head2, n1, entries);
+	}
+
+	printf("head\n");
 	entry *var;
 	SLIST_FOREACH(var, &head, entries) {
+		printf("%i\n", var->value);
+	}
+
+	printf("head2\n");
+	SLIST_FOREACH(var, &head2, entries) {
 		printf("%i\n", var->value);
 	}
 
