aa8cdab803b3362ab06c92da978d8076cca76791
jesg <gowanjason@gmail.com>
2016-05-19 05:18:07 -0700

    need to close channel to get netty example working

--- java/hello-netty/src/main/java/enetty/DiscardServer.java
+++ java/hello-netty/src/main/java/enetty/DiscardServer.java
@@ -1,7 +1,8 @@
 package enetty;
 
 import io.netty.bootstrap.ServerBootstrap;
-
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
@@ -28,17 +29,17 @@ public class DiscardServer {
             ServerBootstrap b = new ServerBootstrap(); // (2)
             b.group(bossGroup, workerGroup)
              .channel(NioServerSocketChannel.class) // (3)
+             .option(ChannelOption.SO_BACKLOG, 100)          // (5)
+             .handler(new LoggingHandler(LogLevel.INFO))
              .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                  @Override
                  public void initChannel(SocketChannel ch) throws Exception {
                      ch.pipeline().addLast(new DiscardServerHandler());
                  }
-             })
-             .option(ChannelOption.SO_BACKLOG, 128)          // (5)
-             .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+             });
 
             // Bind and start to accept incoming connections.
-            ChannelFuture f = b.bind(port).sync(); // (7)
+            ChannelFuture f = b.bind("localhost", port).sync(); // (7)
 
             // Wait until the server socket is closed.
             // In this example, this does not happen, but you can do that to gracefully
--- java/hello-netty/src/main/java/enetty/DiscardServerHandler.java
+++ java/hello-netty/src/main/java/enetty/DiscardServerHandler.java
@@ -2,24 +2,25 @@ package enetty;
 
 import io.netty.buffer.ByteBuf;
 
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
  * Handles a server-side channel.
  */
+@Sharable
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while(in.isReadable()) {
-                System.out.println(in.toString(io.netty.util.CharsetUtil.UTF_8));
-            }
-        } finally {
-            in.release();
-        }
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+        ctx.close(); // why? not in the user guide
     }
 
     @Override
