0ac9f0700e0d0af630f3f5aef765f8f252f57a0b
jesg <gowanjason@gmail.com>
2016-05-21 14:30:07 -0700

    add primitive routing

--- nodejs/simple-proxy.js
+++ nodejs/simple-proxy.js
@@ -3,10 +3,29 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const proxy = function(req, res, host, path, port) {
+error_handler = function(req, res, msg) {
+	res.end();
+	console.warn('WARNING ' + msg);
+	console.info('END ' + req.method + ' ' + res.statusCode + ' ' + req.url);
+}
+const cached_sessions = {};
+const active_sessions = {'1234': {'host': 'localhost', 'port': 8888, 'cap': {}}};
+
+// cleanup on error
+const proxy_session = function(req, res, capture) {
+	const session_id = capture[1];
+	const session_data = active_sessions[session_id];
+	if(!session_data) {
+		res.statusCode = 403;
+		error_handler(req, res, 'unable to find session ' + session_id);
+		return;
+	}
+
+	const host = session_data['host'];
+	const port = session_data['port'];
+	const path = req.url;
 	const destination = host + ':' + port + path;
-	console.info('BEGIN ' + ' forward to ' + destination);
-	callback = (response) => {
+	const callback = (response) => {
 		res.statusCode = response.statusCode;
 		console.info('END ' + ' forward to ' + response.statusCode + ' ' + destination);
 		res.headers = response.headers;
@@ -21,7 +40,7 @@ const proxy = function(req, res, host, path, port) {
 		});
 	}
 
-	var options = {
+	const options = {
 		host: host,
 		path: path,
 		port: port,
@@ -29,7 +48,8 @@ const proxy = function(req, res, host, path, port) {
 		headers: req.headers
 	};
 
-	var proxy_req = http.request(options, callback);
+	console.info('BEGIN ' + ' forward to ' + destination);
+	const proxy_req = http.request(options, callback);
 
 	req.on('data', function(chunk) {
 		proxy_req.write(chunk);
@@ -40,16 +60,41 @@ const proxy = function(req, res, host, path, port) {
 	});
 }
 
+const new_session = 0;
+const quit_session = 0;
+const kill_session = 0;
+const reuse_session = 0;
+const ping = function(req, res, capture) {
+	res.statusCode = 200;
+	res.write('pong');
+	res.end();
+	console.info('END ' + req.method + ' ' + res.statusCode + ' ' + req.url);
+}
+
+const handlers = [
+	[/^\/session$/, new_session],
+	[/^\/session\/([0-9a-z\-]+)$/, quit_session],
+	[/^\/session\/([0-9a-z\-]+)\/kill$/, kill_session],
+	[/^\/session\/([0-9a-z\-]+)\/reuse$/, reuse_session],
+	[/^\/session\/([0-9a-z\-]+)\/[a-z]+/, proxy_session],
+	[/^\/ping$/, ping],
+]
+
 const server = http.createServer((req, res) => {
-	console.info('BEGIN ' + req.method + ' ' + req.url);
+	const url = req.url;
+	console.info('BEGIN ' + req.method + ' ' + url);
 
-	proxy(req, res, 'localhost', req.url, 8888)
-
+	const handlers_len = handlers.length;
+	for(var i = 0; i < handlers_len; i++) {
+		const match = url.match(handlers[i][0]);
+		if(!match) {
+			continue;
+		}
+		handlers[i][1](req, res, match);
+		break;
+	}
 });
 
 server.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
-
-// echo cats
-// curl --data 'cats' http://localhost:3000
